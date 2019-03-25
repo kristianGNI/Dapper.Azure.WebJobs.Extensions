15c1807fb0f28c905663dbb41a02d4e543be4cc1
Kristian Gundry Nielsen <kristiangni@protonmail.com>
2019-03-25 20:47:44 +0100

    Auto stash before merge of "features/samples" and "master"

---
+++ sample/CustomerSample/InsertCustomerSample1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Samples
+{
+    public static class InsertCustomerSample1
+    {
+         [FunctionName("InsertCustomerSample")]
+         [return: Dapper(Sql = "insert.sql", SqlConnection = "SqlConnection")]
+         public static Customer Run([HttpTrigger] Customer customer, ILogger log)
+         {
+             return customer;
+         }
+    }
+}
---
+++ sample/CustomerSample/Model/Customer.cs
@@ -0,0 +1,6 @@
+namespace Samples
+{
+    public int CustomerNumber { get; set; }    
+    public string FirstName { get; set; } 
+    public string LastName { get; set; }
+}
