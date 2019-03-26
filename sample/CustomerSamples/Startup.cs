0946e7ada790dd288391107da2ea3f9746de1e5b
Kristian Gundry Nielsen <kristiangni@protonmail.com>
2019-03-26 10:23:45 +0100

    add all samples

---
+++ sample/CustomerSamples/Startup.cs
@@ -0,0 +1,16 @@
+using Dapper.Azure.WebJobs.Extensions.SqlServer.Config;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.Hosting;
+
+[assembly: WebJobsStartup(typeof(CustomerSamples.Startup))]
+namespace CustomerSamples
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.AddDapperSqlServer();
+        }
+    }
+}
